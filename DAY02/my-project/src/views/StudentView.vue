aa74142e74b21fd0efb3a543bb341dc35d225f15
KyMingH <728750118@qq.com>
2024-06-12 14:58:43 +0800

    vue学习

---
+++ DAY02/my-project/src/views/StudentView.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="myDIV">
+        学生管理页面
+    </div>
+</template>
+
+<script>
+// export default: 这行代码表示导出一个默认对象，使得其他文件可以通过import语句引入这个对象。
+    export default{
+        data(){
+            return{
+
+            }
+        },
+        methods:{
+
+        }
+    }
+</script>
+
+<style>
+.myDIV {
+    color: #42b983;
+}
+</style>
