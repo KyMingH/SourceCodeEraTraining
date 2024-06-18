a10a430c69a1914494b316a4e2562945da601168
KyMingH <728750118@qq.com>
2024-06-18 16:11:40 +0800

    login

---
+++ user.sql
@@ -0,0 +1,36 @@
+/*
+ Navicat Premium Dump SQL
+
+ Source Server         : localhost_3306
+ Source Server Type    : MySQL
+ Source Server Version : 80037 (8.0.37)
+ Source Host           : localhost:3306
+ Source Schema         : cqut
+
+ Target Server Type    : MySQL
+ Target Server Version : 80037 (8.0.37)
+ File Encoding         : 65001
+
+ Date: 18/06/2024 16:10:56
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` int NOT NULL,
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES (1, 'admin', 'admin');
+
+SET FOREIGN_KEY_CHECKS = 1;
